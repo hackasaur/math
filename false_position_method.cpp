448301244e9e0f970d359b5e812fc13ebf0bd13d
Manik <manikraina1096@gmail.com>
2019-08-29 19:17:05 +0530

    Update false_position_method.cpp

--- false_position_method.cpp
+++ false_position_method.cpp
@@ -12,17 +12,18 @@ void regula(double a, double b, double toler){ //a,b are the initial guesses for
   double c = a; 
   int i=1;
   while(f(c) > toler || f(c) < -toler){ //loop will run while f(c) is not in the range of ±toler. 
-  cout<<i<<") "<<"a="<<a<<", b="<<b<<", f(a)="<< f(a) <<", f(b)="<<f(b);  //prints a,b,f(a),f(b) for each iteration(i).
-  c = ( (a*f(b)) - (b*f(a)) )/( f(b) - f(a) );  //calculates value of c using equation of false-position method.
-  if( (f(c) * f(b)) < 0 ){
-    a = c;  
-  }
-  else b = c; //since signs of f(a) nd f(b) must be opposite so that the root lies between [a,b] range.
-    cout<<", c="<<c<<", f(c)="<<f(c)<<endl; //prints f(c) for each itteration.
+    cout<< i << ") " << "a=" << a << ", b=" << b << ", f(a)=" << f(a) << ", f(b)=" << f(b);  //prints a,b,f(a),f(b) for each iteration(i).
+    c = ( (a*f(b)) - (b*f(a)) )/( f(b) - f(a) );  //calculates value of c using equation of false-position method.
+    if( (f(c) * f(b)) < 0 ){
+      a = c;  
+    }
+    else b = c; //since signs of f(a) nd f(b) must be opposite so that the root lies between [a,b] range.
+      cout<<", c="<<c<<", f(c)="<<f(c)<<endl; //prints f(c) for each itteration.
       ++i;
   }
-  cout<<"\nthe root of the equation is: "<<c<<endl; //prints the value of c which is within ±tolerance
+  cout<< "\nthe root of the equation is: " << c << endl; //prints the value of c which is within ±tolerance
 }  
+
 void table_coordinates(double P, double Q, double H){ //prints coordinates after each interval of h from p to q.
   double p = P;
   double q = Q;
@@ -32,6 +33,7 @@ void table_coordinates(double P, double Q, double H){ //prints coordinates after
     p += h;
   }
 }
+
 int main(){
   double a,b,toler = 0.001;
   cout<<"the value of initial guess a:";cin>>a;
